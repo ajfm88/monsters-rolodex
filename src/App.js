1e84f9c471b2ab151f451407a92316142d28b3cd
unknown <ajfoucaultmo@mail.usf.edu>
2022-05-15 14:43:48 -0700

    39. Monsters Rolodex - Searching & Filtering

--- src/App.js
+++ src/App.js
@@ -16,20 +16,39 @@ class App extends Component {
     console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {
-        console.log(this.state);
-      }
-      ));
-    }
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
 
   render() {
     console.log("render");
     return (
       <div className="App">
-        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) => {console.log(event.target.value)}} />
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            // [ { name: 'Leanne}, { name: 'Yihua'} ]
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monsters) => {
           return (
             <div key={monsters.id}>
