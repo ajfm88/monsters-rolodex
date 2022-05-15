b15388fbf801de4756b6dade0879158887cf3010
unknown <ajfoucaultmo@mail.usf.edu>
2022-05-15 13:02:46 -0700

    38. Monsters Rolodex - Input Search Box Component

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log('1');
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log('3');
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState(() => {
@@ -26,9 +26,10 @@ class App extends Component {
     }
 
   render() {
-    console.log('2');
+    console.log("render");
     return (
       <div className="App">
+        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) => {console.log(event.target.value)}} />
         {this.state.monsters.map((monsters) => {
           return (
             <div key={monsters.id}>
