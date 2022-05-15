c0fabe103118b00da1ccc33afeede4e89d2f5c0d
unknown <ajfoucaultmo@mail.usf.edu>
2022-05-14 20:26:23 -0700

    31. Monsters Rolodex - Mapping Arrays to Elements

--- src/App.js
+++ src/App.js
@@ -7,37 +7,29 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: "Alejandro", lastName: "Foucault" },
-      company: "Kojima Productions",
+      monsters: [
+        {
+          name: "Linda",
+        },
+        {
+          name: "Frank",
+        },
+        {
+          name: "Jacky",
+        },
+        {
+          name: "Andrei",
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "Hideo", lastName: "Kojima" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        {this.state.monsters.map((monsters) => {
+          return <h1>{monsters.name}</h1>;
+        })}
       </div>
     );
   }
