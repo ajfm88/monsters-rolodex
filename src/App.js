9b939abf361f49f8416218368615d252452d4e0f
unknown <ajfoucaultmo@mail.usf.edu>
2022-05-17 11:10:36 -0700

    58. Monsters Rolodex - Infinite Re-rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 
 const App = () => {
   const [searchField, setSearchField] = useState(""); // [value, setValue]
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+
+  console.log("render");
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
 
   return (
     <div className="App">
@@ -22,7 +32,7 @@ const App = () => {
         placeholder="search monsters"
       />
 
-      {/* <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
@@ -37,15 +47,15 @@ const App = () => {
 //     };
 //   }
 
-//   componentDidMount() {
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//       .then((response) => response.json())
-//       .then((users) =>
-//         this.setState(() => {
-//           return { monsters: users };
-//         })
-//       );
-//   }
+// componentDidMount() {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((users) =>
+//       this.setState(() => {
+//         return { monsters: users };
+//       })
+//     );
+// }
 
 // onSearchChange = (event) => {
 //   const searchField = event.target.value.toLocaleLowerCase();
@@ -58,10 +68,6 @@ const App = () => {
 //     console.log("render from APPJS");
 //     const { monsters, searchField } = this.state;
 //     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
 
 //     return (
 //       <div className="App">
